759e97f1f360f0d482e23ee8e92080f03704c378
Eric <eric.r.lannan@gmail.com>
2019-08-27 12:51:16 -0700

    Added in a check to make sure a todo was longer than 3 characters before being added to the todoArray

--- Todoy/Controllers/TodoListViewController.swift
+++ Todoy/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    let itemArray = ["Feed Dog", "Walk Dog", "Do some work", "Boxing Workout"]
+    var itemArray = ["Feed Dog", "Walk Dog", "Do some work", "Boxing Workout"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,12 +29,6 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
-//    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-//        if let cell = tableView.cellForRow(at: indexPath) {
-//            cell.accessoryType = .none
-//        }
-//    }
-//    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print(indexPath.row)
         
@@ -42,6 +36,53 @@ class TodoListViewController: UITableViewController {
             cell.accessoryType = cell.accessoryType == .checkmark ? .none : .checkmark
             tableView.deselectRow(at: indexPath, animated: true)
         }
+    }
+    
+    
+    //MARK - Add new items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new Todo",
+                                      message: "",
+                                      preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            if(textField.text!.count > 3) {
+                self.itemArray.append(textField.text!)
+                self.tableView.reloadData()
+            } else {
+                textField.placeholder = "Must be more longer than 3 characters"
+            }
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            
+            alertTextField.addTarget(self, action: #selector(self.textChanged(sender:)), for: .editingChanged)
+            
+            textField = alertTextField
+            
+            
+        }
+        
+        action.isEnabled = false
+        
+        alert.addAction(cancelAction)
+        alert.addAction(action)
+        
+        present(alert,
+                animated: true,
+                completion: nil)
+    }
+    
+     @objc func textChanged(sender:UITextField) {
+        let tf = sender
+        var resp : UIResponder = tf
+        while !(resp is UIAlertController) { resp = resp.next! }
+        let alert = resp as! UIAlertController
+        (alert.actions[1] as UIAlertAction).isEnabled = (tf.text!.count > 3)
     }
 }
 
